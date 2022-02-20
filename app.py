61b294e6e3c6ae33c8e4821a693167efd063d9b7
Abel Diress <abeldiress05@gmail.com>
2022-02-20 06:00:15 -0500

    some quick ui additions and changes

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
+from importlib.metadata import requires
 from werkzeug import Response
-from flask import Flask, render_template, url_for
+from flask import Flask, redirect, render_template, url_for, request
 import cv2
 
 app = Flask(__name__)
 
 cam = cv2.VideoCapture(0)
+
+reps = 0
+sets = 0
+excercise = ''
+time = 0
 
 def captureFrame():
     while True:            
@@ -18,9 +24,22 @@ def captureFrame():
     yield (b'--frame\r\n'
             b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    global reps, sets, pause, excercise
+
+    if request.method == 'POST':
+        reps = request.form['reps']
+        sets = request.form['sets']
+        pause = request.form['pause']
+        excercise = request.form['excerise']
+
+        return redirect(url_for('repCounter'))
+    return render_template('initial.html')
+
+@app.route('/reps')
+def repCounter():
+    return render_template('index.html', reps=reps, sets=sets, pause=pause, excercise=excercise)
 
 @app.route('/video')
 def video():
